c145794cc047738f36c3fc96a5cf2ee43e9a40cf
Ubuntu <ubuntu@ip-172-31-39-148.ap-southeast-4.compute.internal>
2025-11-12 07:58:17 +0000

    body of conv inner loops

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -7,6 +7,46 @@ import neuronxcc.nki.isa as nisa
 from neuronxcc.nki import baremetal
 
 
+@nki.compiler.skip_middle_end_transformations
+@nki.jit
+def nki_matmul_tiled_(lhsT, rhs, result):
+  """NKI kernel to compute a matrix multiplication operation in a tiled manner"""
+
+  K, M = lhsT.shape
+  K_, N = rhs.shape
+  assert K == K_, "lhsT and rhs must have the same contraction dimension"
+
+  # Maximum free dimension of the stationary operand of general matrix multiplication on tensor engine
+  TILE_M = nl.tile_size.gemm_stationary_fmax  # 128
+
+  # Maximum partition dimension of a tile
+  TILE_K = nl.tile_size.pmax  # 128
+
+  # Maximum free dimension of the moving operand of general matrix multiplication on tensor engine
+  TILE_N = nl.tile_size.gemm_moving_fmax  # 512
+
+  # Use affine_range to loop over tiles
+  for m in nl.affine_range(M // TILE_M):
+    for n in nl.affine_range(N // TILE_N):
+      # Allocate a tensor in PSUM
+      res_psum = nl.zeros((TILE_M, TILE_N), nl.float32, buffer=nl.psum)
+
+      for k in nl.affine_range(K // TILE_K):
+        # Declare the tiles on SBUF
+        lhsT_tile = nl.ndarray((TILE_K, TILE_M), dtype=lhsT.dtype, buffer=nl.sbuf)
+        rhs_tile = nl.ndarray((TILE_K, TILE_N), dtype=rhs.dtype, buffer=nl.sbuf)
+
+        # Load tiles from lhsT and rhs
+        nisa.dma_copy(dst=lhsT_tile, src=lhsT[k * TILE_K:(k + 1) * TILE_K, m * TILE_M:(m + 1) * TILE_M])
+        nisa.dma_copy(dst=rhs_tile, src=rhs[k * TILE_K:(k + 1) * TILE_K, n * TILE_N:(n + 1) * TILE_N])
+
+        # Accumulate partial-sums into PSUM
+        res_psum += nisa.nc_matmul(lhsT_tile[...], rhs_tile[...])
+
+      # Copy the result from PSUM back to SBUF, and cast to expected output data-type
+      res_sb = nl.copy(res_psum, dtype=result.dtype)
+      nisa.dma_copy(dst=result[m * TILE_M:(m + 1) * TILE_M, n * TILE_N:(n + 1) * TILE_N], src=res_sb)
+
 """
 A fused convolution - maxpool kernel that you need to implement for Part 2.
 
@@ -72,16 +112,31 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
 
     # Process the images in batches
     for b in nl.affine_range(batch_size):
-        # To start, assume the images are small (they can fit in SBUF) and move the entire image to SBUF
+        # TODO: Chunk image to not load whole thing at once
         img_sbuf = nl.ndarray((in_channels, input_height * input_width))
         nl.dma_copy(dst=img_sbuf, src=X_res[b], dtype=X.dtype)
 
-        out_sbuf = nl.ndarray((out_channels, out_height * out_width))
+        conv_out_psum = nl.zeros((out_channels, in_channels), nl.float32, buffer=nl.psum)
+
+        conv_out_sbuf = nl.ndarray(out_channels, input_height * input_width)
+        out_sbuf = nl.ndarray((out_channels, out_pool_height * out_pool_width))
 
         # Perform the convolution
-        for i in nl.affine_range(filter_height):
-            for j in nl.affine_range(filter_width):
-                img_sbuf_shifted = 
+        for n in nl.affine_range(filter_height):
+            for m in nl.affine_range(filter_width):
+                shifted_img_sbuf = np.roll(img_sbuf, - (n * input_width + m))
+
+                weights_sbuf = nl.ndarray((out_channels, in_channels))
+                nl.dma_copy(dst=weights_sbuf, src=W[:, :, n, m])
+
+                conv_out_psum += nisa.nc_matmul(weights_sbuf[...], shifted_img_sbuf[...])
+        
+        conv_out_sbuf = nl.copy(conv_out_psum, dtype=X_out.dtype)
+        conv_out_sbuf_res = conv_out_sbuf.reshape((out_channels, input_height, input_width))
+        
+        conv_out = conv_out_sbuf[:, :out_height, :out_width]
+        
+        pool_out = nisa.tensor_reduce(nl.max, conv_out, )
 
         # Accumulate the results in output
 
