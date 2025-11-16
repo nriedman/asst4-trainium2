9442f1843c1a0220305186ff5008154851428c71
Nick Riedman <nriedman@stanford.edu>
2025-11-16 13:31:46 +0000

    debugging

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -73,16 +73,21 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     hw_pmax = nl.tile_size.pmax
     n_tiles_hw = (input_height * input_width) // hw_pmax
 
+    w_pmax = nl.tile_size.pmax
+    n_tiles_w = (input_width + w_pmax - 1) // w_pmax
+
+    pool_tile_height = 2 // pool_size
+    pool_tile_width = hw_pmax // pool_size
+
     # Load W into sbuf and transpose
     W_res = W.reshape((c_out_pmax, c_in_pmax, n_tiles_c_out, n_tiles_c_in, filter_height, filter_width))
     W_T = nl.ndarray((c_in_pmax, c_out_pmax, n_tiles_c_out, n_tiles_c_in, filter_height, filter_width), dtype=W.dtype, buffer=nl.sbuf)
 
-    bias_res = bias.reshape((c_out_pmax, n_tiles_c_out))
     B_sbuf = nl.ndarray((c_out_pmax, n_tiles_c_out), dtype=bias.dtype, buffer=nl.sbuf)
     
     for c_out_tile in nl.affine_range(n_tiles_c_out):
         # load in bias while we're at it
-
+        nisa.dma_copy(src=bias[c_out_tile*c_out_pmax:(c_out_tile+1)*c_out_pmax], dst=B_sbuf[:, c_out_tile])
 
         for c_in_tile in nl.affine_range(n_tiles_c_in):
             for i in nl.affine_range(filter_height):
@@ -116,11 +121,34 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
                             w_T = W_T[:, :, c_out_tile, c_in_tile, i, j]
 
                             # Load in shifted image input
-                            x_in = nl.ndarray((c_in_pmax, hw_pmax), dtype=X.dtype, buffer=nl.sbuf)
+                            x_in = nl.zeros((c_in_pmax, 2, hw_pmax), dtype=X.dtype, buffer=nl.sbuf)
 
-                            pass
+                            h_start = hw_tile_pair
 
-                pool_tile_out = nisa.tensor_scalar(conv_tile_out_psum, nl.add, bias[c_out_tile*c_out_pmax:(c_out_tile+1)*c_out_pmax])
+                            conv_tile_out_psum += nisa.nc_matmul(w_T[...], x_in[...])
+
+                # Collect the accumulated convolution tile
+                # Shape: (c_out_pmax, 2, hw_pmax)
+                pool_tile_out = nisa.tensor_scalar(conv_tile_out_psum, nl.add, B_sbuf[:, c_out_tile])
+
+                if pool_size > 1:
+                    pool_tile_out_res = pooL_tile_out.reshape((c_out_pmax, 1, 2, hw_pmax//2, 2))
+                    pool_tile_out = nisa.tensor_reduce(nl.max, pool_tile_out_res, axis=[2,4])
+                
+                # Pool out has the convolution with the bias + max pooling
+                # Shape: (c_out_pmax, 2 // pool_size, hw_pmax // pool_size)
+
+                # At this point, we can go ahead and write the results to HBM
+                out_width_start = (hw_tile_pair % n_tiles_w) * pool_tile_width
+                out_width_end = out_width_start + pool_tile_width
+
+                out_height_start = (hw_tile_pair // n_tiles_w) * pool_tile_height
+                out_height_end = out_height_start + pool_tile_height
+                
+                nisa.dma_copy(
+                    src=pool_tile_out,
+                    dst=X_out[b, c_out_tile*c_out_pmax:(c_out_tile+1)*c_out_pmax, out_height_start:out_height_end, out_width_start:out_width_end]
+                )
 
     return X_out
 
